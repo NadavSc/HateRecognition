d1eb2c70404777bc3ddf84d3b370da1ce36da9c8
NadavSc <nadavsch19ad@gmail.com>
2023-01-20 17:18:00 +0200

    1. added working preprocess 2. working training framework in Pytorch

--- model1/__init__.py
+++ model1/__init__.py
@@ -1,33 +1,69 @@
 import os
-import pandas as pd
+import re
 import torch
+import pdb
+import gensim
 import numpy as np
+import pandas as pd
 
 from torch import nn
 from transformers import BertModel, BertTokenizer
 from os.path import abspath, dirname
+from nltk import WordNetLemmatizer
 
 
 PATH = dirname(dirname(abspath(__file__)))
 DATA_DIR = os.path.join(PATH, 'data')
 annotated_data_path = os.path.join(DATA_DIR, 'parler_annotated_data.csv')
 annotated_labeled_data_path = os.path.join(DATA_DIR, 'parler_annotated_data_labeled.csv')
-annotated_prep_data_path = os.path.join(DATA_DIR, 'parler_annotated_data_prep.csv')
 np.random.seed(42)
 
 
-def insert_hate_label():
+def lemmatize(token):
+    return WordNetLemmatizer().lemmatize(token, pos='v')
+
+
+def preprocess(tweet):
+    result = re.sub(r'(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)
+    result = re.sub(r'(@[A-Za-z0-9-_]+)', '', result)
+    result = re.sub(r'http\S+', '', result)
+    result = re.sub(r'bit.ly/\S+', '', result)
+    result = re.sub(r'(.)\1+', r'\1\1', result)
+    # result = " ".join(re.findall('[A-Z][^A-Z]*', result))
+    result = re.sub(r'&[\S]+?;', '', result)
+    result = re.sub(r'#', ' ', result)
+    result = re.sub(r'[^\w\s]', r'', result)
+    result = re.sub(r'\w*\d\w*', r'', result)
+    result = re.sub(r'\s\s+', ' ', result)
+    result = re.sub(r'(\A\s+|\s+\Z)', '', result)
+    # result = tokenize(result)
+    return result
+
+
+def tokenize(tweet):
+    result = []
+    for token in gensim.utils.simple_preprocess(tweet):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 2:
+            result.append(lemmatize(token))
+    res = ' '.join(result)
+    return res
+
+
+def insert_hate_label(threshold):
     dataset = pd.read_csv(annotated_data_path)
-    label = [1 if label_mean >= 3.0 else 0 for label_mean in dataset['label_mean']]
+    label = [1 if label_mean >= threshold else 0 for label_mean in dataset['label_mean']]
     dataset.insert(1, "label", label, True)
-    dataset.to_csv('parler_annotated_data_labeled.csv', index=False)
+    dataset.to_csv(f'{DATA_DIR}/parler_annotated_data_labeled_{threshold}.csv', index=False)
     return dataset
 
 
-def prep_train():
-    dataset = insert_hate_label()
+def prep_train(threshold=3, tokenizer=False):
+    dataset = insert_hate_label(threshold)
+    if tokenizer:
+        dataset['text'] = dataset['text'].apply(preprocess)
+    dataset.drop(dataset.index[(dataset['text'] == '')], axis=0, inplace=True)
     dataset = dataset[['label', 'text']]
-    dataset.to_csv('parler_annotated_data_prep.csv', index=False)
+    dataset.to_csv(f'{DATA_DIR}/parler_annotated_data_prep_{threshold}.csv', index=False)
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -60,7 +96,7 @@ class Dataset(torch.utils.data.Dataset):
 
 
 class BertClassifier(nn.Module):
-    def __init__(self, dropout=0.5):
+    def __init__(self, dropout=0.7):
         super(BertClassifier, self).__init__()
 
         self.bert = BertModel.from_pretrained('bert-base-cased')
@@ -69,12 +105,9 @@ class BertClassifier(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, input_id, mask):
-        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask, return_dict=False)
         dropout_output = self.dropout(pooled_output)
         linear_output = self.linear(dropout_output)
         final_layer = self.relu(linear_output)
 
         return final_layer
-
-df = pd.read_csv(annotated_prep_data_path)
-df_train, df_test = np.split(df.sample(frac=1, random_state=42), [int(.8*len(df))])
--- model1/train.py
+++ model1/train.py
@@ -1,31 +1,31 @@
+import os
+import sys
+from os.path import dirname, abspath
+
+project_path = dirname(dirname(abspath(__file__)))
+model1_dir = os.path.join(project_path, 'model1')
+sys.path.append(project_path)
+sys.path.append(model1_dir)
+
 import torch
+import pdb
+import pickle
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from torch import nn
 from torch.optim import Adam
 from tqdm import tqdm
 
-from model1 import df_train, df_test
-from model1 import BertClassifier, Dataset
+from model1 import BertClassifier, Dataset, DATA_DIR, prep_train
 
 
-EPOCHS = 5
-BATCH_SIZE = 8
-LR = 1e-6
-
-
-def train(model, train_data, val_data):
+def train(model, train_data, val_data, criterion, optimizer):
     train, val = Dataset(train_data), Dataset(val_data)
 
     train_dataloader = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
     val_dataloader = torch.utils.data.DataLoader(val, batch_size=BATCH_SIZE)
-
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
-    print(f'{device} is running')
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=LR)
 
     if use_cuda:
         model = model.cuda()
@@ -49,11 +49,9 @@ def train(model, train_data, val_data):
 
             batch_loss = criterion(output, train_label.long())
             total_loss_train += batch_loss.item()
-            hist_total_loss_train.append(total_loss_train)
 
             acc = (output.argmax(dim=1) == train_label).sum().item()
             total_acc_train += acc
-            hist_total_acc_train.append(acc)
 
             model.zero_grad()
             batch_loss.backward()
@@ -72,22 +70,32 @@ def train(model, train_data, val_data):
 
                 batch_loss = criterion(output, val_label.long())
                 total_loss_val += batch_loss.item()
-                hist_total_loss_val.append(total_loss_val)
 
                 acc = (output.argmax(dim=1) == val_label).sum().item()
                 total_acc_val += acc
-                hist_total_acc_val.append(total_acc_val)
 
         print(
             f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
                 | Train Accuracy: {total_acc_train / len(train_data): .3f} \
                 | Val Loss: {total_loss_val / len(val_data): .3f} \
                 | Val Accuracy: {total_acc_val / len(val_data): .3f}')
+        hist_total_acc_train.append(total_acc_train/len(train_data))
+        hist_total_loss_train.append(total_loss_train/len(train_data))
+        hist_total_acc_val.append(total_acc_val/len(val_data))
+        hist_total_loss_val.append(total_loss_val/len(val_data))
 
     return model, hist_total_acc_train, hist_total_loss_train, hist_total_acc_val, hist_total_loss_val
 
 
-def plot(loss_train, loss_val, acc_train, acc_val):
+def plot(save_dir):
+    with open(f'{save_dir}/acc_train.pkl', 'rb') as f:
+        acc_train = pickle.load(f)
+    with open(f'{save_dir}/loss_train.pkl', 'rb') as f:
+        loss_train = pickle.load(f)
+    with open(f'{save_dir}/acc_val.pkl', 'rb') as f:
+        acc_val = pickle.load(f)
+    with open(f'{save_dir}/loss_val.pkl', 'rb') as f:
+        loss_val = pickle.load(f)
     fig, axis = plt.subplots(nrows=1, ncols=2)
     axis[0].plot(loss_train, label='train')
     axis[0].plot(loss_val, label='val')
@@ -104,10 +112,46 @@ def plot(loss_train, loss_val, acc_train, acc_val):
     plt.show()
 
 
+def save_results(save_dir):
+    with open(f'{save_dir}/acc_train.pkl', 'wb') as f:
+        pickle.dump(acc_train, f)
+    with open(f'{save_dir}/loss_train.pkl', 'wb') as f:
+        pickle.dump(loss_train, f)
+    with open(f'{save_dir}/acc_val.pkl', 'wb') as f:
+        pickle.dump(acc_val, f)
+    with open(f'{save_dir}/loss_val.pkl', 'wb') as f:
+        pickle.dump(loss_val, f)
+
+
+EPOCHS = 5
+BATCH_SIZE = 8
+LR = 1e-6
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+print(f'{device} is running')
+
+threshold = 3
+prep_train(threshold=threshold, tokenizer=False)
+annotated_prep_data_path = f'{DATA_DIR}/parler_annotated_data_prep_{threshold}.csv'
+df = pd.read_csv(annotated_prep_data_path)
+df_train, df_test = np.split(df.sample(frac=1, random_state=42), [int(.8*len(df))])
+
+
 model = BertClassifier()
+# weight = torch.tensor([0.7, 1.2]).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=LR)
 model, acc_train, loss_train, acc_val, loss_val = train(model=model,
                                                         train_data=df_train,
-                                                        val_data=df_test)
-plot(loss_train, loss_val, acc_train, acc_val)
-
-
+                                                        val_data=df_test,
+                                                        criterion=criterion,
+                                                        optimizer=optimizer)
+try:
+    save_dir = os.path.join(model1_dir, f'running1')
+    os.mkdir(os.path.join(model1_dir, 'running1'))
+except:
+    idx = max([int(fname[-1]) for fname in os.listdir(model1_dir) if 'running' in fname])
+    save_dir = os.path.join(model1_dir, f'running{idx+1}')
+    os.mkdir(save_dir)
+save_results(save_dir)
+# plot(save_dir)
